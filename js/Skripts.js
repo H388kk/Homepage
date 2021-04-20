ec343e203e82c64de326e516dc07bf8447307fff
H388kk <78017274+H388kk@users.noreply.github.com>
2021-04-20 16:23:19 +0300

    Vienkrsi saglabaju veikto darbu

--- js/Skripts.js
+++ js/Skripts.js
@@ -1,9 +1,25 @@
 console.log("Skripts stradā!");
 
 document.getElementById("bobaBo").addEventListener("click", pogaStrada);
+let bibliotec = []
 
 function pogaStrada() {
     console.log("Poga stradā");
     let popUp = document.getElementById("popUp");
     popUp.style.display = "block"
+}
+
+document.getElementById("jaunsProdukts").addEventListener("click", jaunsProdukts);
+
+function jaunsProdukts() {
+    console.log("Pievienot Produktu")
+
+    let popUp = document.getElementById("popUp");
+    popUp.style.display = "none"
+
+    let produkts = {produktaValue: produktaVards.value}
+
+    bibliotec.push(produkts)
+    console.log(bibliotec);
+
 }
